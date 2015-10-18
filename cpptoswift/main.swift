7b16d1485bac8a4e73e8e5e222320c4ba1068873
Uli Kusterer <ulivc@zathras.de>
2015-10-18 15:19:02 +0200

    Initial Commit

---
+++ cpptoswift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cpptoswift
+//
+//  Created by Uli Kusterer on 18/10/15.
+//  Copyright © 2015 Uli Kusterer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
